e16d6b759f148e028a10915d24f5c2371c15cda6
Yuri Nunes <yuri.cruz10@hotmail.com>
2025-02-09 11:41:51 -0300

    Inicializando app

---
+++ src/test/java/br/com/ydcns/AppPessoas/AppPessoasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ydcns.AppPessoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppPessoasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
